515e9b5336390bf2c76feebc7d3e079afa0cbe96
Matt Brazza <mattbrazza@gmail.com>
2017-07-27 17:57:19 -0400

    Completing the profile editor

--- app/profileCtrl.js
+++ app/profileCtrl.js
@@ -1,8 +1,17 @@
 // PROFILE-EDITOR CONTROLLER
 anApp.controller('profileCtrl',
-['userService','$scope','$http',
-function(userService, $scope, $http){
+['userService','$scope','$http','$location',
+function(userService, $scope, $http, $location){
 
+  /* Redirect non-logged-in users */
+  if (!userService.isLoggedIn()) {
+    console.log('User not logged-in');
+    $location.path('/');
+  } else {
+    $scope.user = userService.getUser();
+  }
+
+  function updateProfilePic(){};
 
 }]);
 
